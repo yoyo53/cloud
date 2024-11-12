f2a3c63270d4f88cc9711ea353cc4bf54991bc68
Yohan Villiers <94387950+yoyo53@users.noreply.github.com>
2024-11-12 23:34:49 +0100

    add kubernetes config for JS service

--- rentalService/build-docker.sh
+++ rentalService/build-docker.sh
@@ -1,2 +1,2 @@
 docker build -t ghcr.io/yoyo53/rental-service .
-docker push ghcr.io/yoyo53/rental-service
+docker push ghcr.io/yoyo53/rental-service:latest
---
+++ rentalServiceJS/build-docker.sh
@@ -0,0 +1,2 @@
+docker build -t ghcr.io/yoyo53/rental-service-js .
+docker push ghcr.io/yoyo53/rental-service-js:latest
